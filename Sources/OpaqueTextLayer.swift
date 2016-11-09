e7ba465361dbedbff1758baa1b64a7246ce81e3d
CodeEagle <stasura@hotmail.com>
2016-11-09 10:50:40 +0800

    add default background color

--- Sources/OpaqueTextLayer.swift
+++ Sources/OpaqueTextLayer.swift
@@ -26,6 +26,7 @@ open class OpaqueTextLayer: CATextLayer {
     
     private func initialize() {
         contentsScale = UIScreen.main.scale
+        backgroundColor = UIColor.white.cgColor
         isOpaque = true
     }
     
